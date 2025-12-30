a408598d6352d7d4be28675d9ed2cea951903c00
pavansai018 <18pavansai@gmail.com>
2025-12-31 01:30:15 +0800

    added variable file

---
+++ src/v1/variables.py
@@ -0,0 +1,15 @@
+# Live feed variables
+ENABLE_DISPLAY = True
+WINDOW_NAME = 'PathPal Live'
+DISPLAY_WIDTH = 1280
+DISPLAY_HEIGHT = 720
+
+# models path
+COCO_SSD_MOBILENET_V1_PATH = 'models/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29/detect.tflite'
+COCO_LABELS_PATH = 'models/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29/labelmap.txt'
+
+# camera settings
+CAM_WIDTH = 320
+CAM_HEIGHT = 240
+FPS = 15
+
